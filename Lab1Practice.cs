5efa77efa34e8bdd025f1e3b831692e2c6c3de9e
rkelepolo <ryankelepolo@gmail.com>
2022-09-07 22:47:18 -0600

    Practice

---
+++ Lab1Practice.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Ryan";
+      Console.WriteLine(name);  
+      int number = 11;
+      Console.WriteLine(number);
+     }
+  }
+}
