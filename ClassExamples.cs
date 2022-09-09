73ad3b9adbe359281676fa45f86d2b8f744986ed
rkelepolo <112348047+rkelepolo@users.noreply.github.com>
2022-09-09 11:30:18 -0600

    commit for Unit1A Challenge

---
+++ ClassExamples.cs
@@ -0,0 +1,31 @@
+public class Class Example
+{
+
+}
+
+public class ClassWithVariables
+{
+    public int number = 2;
+    public string userName = "Ryan";
+    public double DoubleNumber = 13.5;
+    public bool MyBool = true;
+}
+
+public class ClassWithFunctions
+{
+    public void RunFunction()
+    {
+        Console.Writeline("Running Function");
+    }
+}
+{
+    static void MyMethod()
+    {
+        Console.Writeline("Woah there partner");
+    }
+}  
+
+static void Main(string[] args)
+{
+    MyMethod();
+}
