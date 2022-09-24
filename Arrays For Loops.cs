9b6dbcf5a42d5eb4e533179da2e29711d23bdd5e
rkelepolo <ryankelepolo@gmail.com>
2022-09-23 21:05:36 -0600

    Changed pawn to queen

--- Arrays For Loops.cs
+++ Arrays For Loops.cs
@@ -5,6 +5,8 @@ public class Program
     public static void Main()
     {
         string[] playerTypes = {"Pawn", "Pawn", "Pawn"};
+        playerTypes[1] = "Queen";
+
         foreach(var playerTyper in playerTypes) {
             Console.Writeline(playerType);
         }
