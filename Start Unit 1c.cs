7aa3c187dab7775a8ee501acce6fea67d543b8dd
rkelepolo <ryankelepolo@gmail.com>
2022-09-23 20:51:39 -0600

    Started Unit 1c

---
+++ Start Unit 1c.cs
@@ -0,0 +1,16 @@
+using System
+
+public class Program
+{
+        public void Main()
+        {
+            string{} cars = {"Mustiang", "Camero", "Hellcat", "Corvette, "Viper", "McClarren"}
+            Console.Writeline(cars.Length);
+            Console.Writeline(cars[0]);
+            Console.Writeline(cars[1]);
+            Console.Writeline(cars[2]);
+            Console.Writeline(cars[3]);
+            Console.Writeline(cars[4]);
+            Console.Writeline(cars[5]);
+        }
+}
