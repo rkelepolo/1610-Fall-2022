40efa3eee2de839b65f4d99df7bca864b674431a
rkelepolo <ryankelepolo@gmail.com>
2022-09-23 21:03:21 -0600

    Created arrays for loops

---
+++ Arrays For Loops.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] playerTypes = {"Pawn", "Pawn", "Pawn"};
+        foreach(var playerTyper in playerTypes) {
+            Console.Writeline(playerType);
+        }
+    }
+}
