9adda90296ad79b9e51ec4d9afc8fff92cb399fc
rkelepolo <ryankelepolo@gmail.com>
2022-09-23 21:25:19 -0600

    Axe, sword, hammer all with power level 2

--- Arrays With Classes and Loops.cs
+++ Arrays With Classes and Loops.cs
@@ -3,11 +3,18 @@ using System;
 public class Program{
     public static void Main()
     {
-        weapon[] weaponsObjs = {new weapon(), new weapon()};
+        weapon[] weaponsObjs = {new weapon(), new weapon(), new weapon()};
         weaponsObjs[1].weaponName = "sword";
+        weaponsObjs[2].weaponName = "Hammer";
+
+        for(var i=0;i<weaponsObjs.Length;i++) {
+            weaponsObjs[i].powerLevel = 2;
+
+        }
 
         foreach(var item in weaponsObjs){
             Console.Writeline(item.weaponName);
+            Console.WritLine(item.powerLevel);
         }
     }
 }
