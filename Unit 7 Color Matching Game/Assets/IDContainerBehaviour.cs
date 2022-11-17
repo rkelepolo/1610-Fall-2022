79d43861eeb253850359e23491a311176291b61a
rkelepolo <ryankelepolo@gmail.com>
2022-11-17 13:04:23 -0700

    Unit 7

---
+++ Unit 7 Color Matching Game/Assets/IDContainerBehaviour.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class IDContainerBehaviour : MonoBehaviour
+{ 
+    public ID idObj;
+}
---
+++ Unit 7 Color Matching Game/Assets/MatchBehaviour.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MatchBehaviour : MonoBehaviour
+{
+   public ID idObj;
+
+   private void OnTriggerEnter(Collider other)
+   {
+       var otherID = other.GetComponent<IDContainerBehaviour>().idObj;
+       if (otherID == idObj)
+       {
+           Debug.Log(message: "Matched");
+       }    
+   }
+}
